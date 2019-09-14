a8a3e9e9fcc11cbde1e7c8e952cef9fd0ba7a637
dillonharless18 <43771091+dillonharless18@users.noreply.github.com>
2019-09-14 18:00:38 -0400

    Started image view

--- js/agency.js
+++ js/agency.js
@@ -7,9 +7,18 @@
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 54)
-        }, 1000, "easeInOutExpo");
+        // midsize and desktop scroll
+        if ($( window ).width() >= 550) {
+          $('html, body').animate({
+            scrollTop: (target.offset().top - 54)
+          }, 1000, "easeInOutExpo");
+        }
+        // mobile scroll
+        else {
+          $('html, body').animate({
+            scrollTop: (target.offset().top - 83)
+          }, 1000, "easeInOutExpo");
+        }
         return false;
       }
     }
